54a6535ab372139e87b18c19f00703230a6355e6
srijitcn <srijitcn>
2024-11-18 17:38:53 +0000

    Done with RAG tool

--- prep/03 Create Tools and Agent.py
+++ prep/03 Create Tools and Agent.py
@@ -51,7 +51,9 @@ class RetrieverConfig(BaseModel):
     vector_search_endpoint_name:str
     vector_index_name:str
     vector_index_id_column:str
+    num_documents_to_retrieve:int = 1
     retrieve_columns:List[str]
+
 
 #a base class for all tools. simplifies structured tool creation
 class BaseToolBuilder:
@@ -146,10 +148,11 @@ class TextClassifier(BaseToolBuilder):
 
 # MAGIC %md
 # MAGIC ### RAG Tool
+# MAGIC A tool that helps you perform Retrieval Augmented Generation based on a query
 
 # COMMAND ----------
 
-class VectorIndexRetriever():    
+class DBVectorIndexRetriever():    
     """A retriever class to do Databricks Vector Index Search"""
     retriever_config: RetrieverConfig = None
     vector_index: VectorSearchIndex = None
@@ -158,76 +161,123 @@ class VectorIndexRetriever():
         super().__init__()
         self.retriever_config = retriever_config
         
-        vsc = VectorSearchClient()
+        vsc = VectorSearchClient(disable_notice=True)
         
         self.vector_index = vsc.get_index(endpoint_name=self.retriever_config.vector_search_endpoint_name,
                                           index_name=self.retriever_config.vector_index_name)
 
-    @mlflow.trace(name="get_benefit_retriever", span_type="func")
-    def get_benefits(self, client_id:str, question:str):
+    @mlflow.trace(name="retrieve", span_type="func")
+    def retrieve(self, query:str, filters:dict = {}):
         query_results = self.vector_index.similarity_search(
-            query_text=question,
-            filters={"client":client_id},
+            query_text=query,
+            filters=filters,
             columns=self.retriever_config.retrieve_columns,
-            num_results=1)
+            num_results=self.retriever_config.num_documents_to_retrieve)
         
         return query_results
 
 
-class BenefitsRAGInput(BaseModel):
+class RAGInput(BaseModel):
     """Data class for tool input"""
-    client_id : str = Field(description="Client ID for which the benefits need to be retrieved")
-    question: str = Field(description="Question for which the benefits need to be retrieved")
+    filters : dict = Field(description="This is a python dictionary with key value pairs as additional filters to be applied to the search")
+    query: str = Field(description="Query for which the documents need to be retrieved")
 
-class Benefit(BaseModel):
-    """Data class for tool output"""
-    text:str = Field(description="Full text as provided in the context as-is without changing anything")
-    in_network_copay:float = Field(description="In Network copay amount. Set to -1 if not covered or has coinsurance")
-    in_network_coinsurance:float= Field(description="In Network coinsurance amount without the % sign. Set to -1 if not covered or has copay")
-    out_network_copay:float = Field(description="Out of Network copay amount. Set to -1 if not covered or has coinsurance")
-    out_network_coinsurance:float = Field(description="Out of Network coinsurance amount without the % sign. Set to -1 if not covered or has copay")
+#This class need to be extended appropriately for different applications
+class BaseRAG(BaseToolBuilder):
+    """Tool class implementing a simple Retrieval Augmented Generation pipeline"""
+    #implementing class must define
+    #name : str = give proper name
     
-class BenefitsRAG(BaseCareCostToolBuilder):
-    """Tool class implementing the benefits retriever"""
-    name : str = "BenefitsRAG"
-    description : str = "useful for retrieving benefits from a vector search index in json format"
-    args_schema : Type[BaseModel] = BenefitsRAGInput
+    #implementing class must define
+    #description : str = Give a proper description
+    
+    #implementing class must define
+    #args_schema : Type[BaseModel] = provide the base class for inputs
+
     model_endpoint_name:str = None
     retriever_config: RetrieverConfig = None    
     retrieved_documents:List[Document] = None
-    prompt_coverage_qa:str = "Get the member medical coverage benefits from the input sentence at the end:\
-        The output should only contain the formatted JSON instance that conforms to the JSON schema below.\
-        Do not provide any extra information other than the json object.\
-        {pydantic_parser_format_instruction}\
-        Input Sentence:{context}"
-    
 
     def __init__(self,
                  model_endpoint_name : str,
+                 prompt:str,
                  retriever_config: RetrieverConfig):
         super().__init__()
         self.model_endpoint_name = model_endpoint_name
+        self.prompt = prompt
         self.retriever_config = retriever_config
-        
-    @mlflow.trace(name="get_benefits", span_type="func")
-    def execute(self, client_id:str, question:str) -> str:
+    
+    def get_prompt_variables(self) -> dict:
+      raise NotImplementedError("This method should be implemented by child class")
 
-        retriever = BenefitsRetriever(self.retriever_config)        
+    @mlflow.trace(name="base_rag_execute", span_type="func")
+    def execute(self, query:str, filters:dict={}) -> str:
+
+        retriever = DBVectorIndexRetriever(self.retriever_config)        
         self.retrieved_documents = None
-        query_results = retriever.get_benefits(client_id, question)
+        query_results = retriever.retrieve(query=query, filters=filters)
         
         if query_results["result"]["row_count"] > 0:
-            coverage_records = [Document(page_content=data[1]) for data in query_results["result"]["data_array"]]
+            context_docs = []
+            for data in query_results["result"]["data_array"]:              
+              record_str = ""
+              for i in range(len(self.retriever_config.retrieve_columns)):
+                col_name = self.retriever_config.retrieve_columns[i]
+                col_data = data[i]
+                record_str += f"{col_name}: {col_data if col_data else 'No Data'} \n"
+              
+              context_docs.append(Document(page_content=record_str))
+
             #save the records for evaluation
-            self.retrieved_documents = coverage_records
+            self.retrieved_documents = context_docs
 
             qa_chain = build_api_chain(model_endpoint_name=self.model_endpoint_name,
-                                       prompt_template=self.prompt_coverage_qa,
+                                       prompt_template=self.prompt,
                                        qa_chain=True)
-            parser = PydanticOutputParser(pydantic_object=Benefit)
-
-            answer = qa_chain.invoke({"context": coverage_records,
-                               "pydantic_parser_format_instruction": parser.get_format_instructions()})
-            return answer.replace('`','')# Benefit.model_validate_json(answer)
+            
+            prompt_variables = self.get_prompt_variables()
+            prompt_variables.update({"context": context_docs})
+            prompt_variables.update({"question": query})
+            
+            answer = qa_chain.invoke(prompt_variables)
+            return answer
         else:
-            raise Exception("No coverage found")
+            return "Sorry no records were found related to the query that satisfies the given filters."
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ####Usage
+
+# COMMAND ----------
+
+#implement a RAG to answer question on Covid Trials based on a query on title
+class CovidTrialTitleRAG(BaseRAG):
+  name : str = "CovidTrialTitleRAG"
+  description : str = "Retrieves Covid Trial documents based on title"
+  args_schema : Type[BaseModel] = RAGInput
+  prompt:str = "Answer the question based on the text available in the context.\
+        Always append all the URL of the documents in the end as separate bulleted list. \n\
+        Question: {question} \n\
+        Context:{context}"
+
+  num_documents:int = 3
+
+  def __init__(self, model_endpoint_name : str, retriever_config: RetrieverConfig):
+    super().__init__(model_endpoint_name=model_endpoint_name,
+                      prompt=self.prompt,
+                      retriever_config=retriever_config)
+    
+  def get_prompt_variables(self) -> dict:
+    #since we are not setting any more variables in the prompt other than context, we can return an empty dict
+    return {}
+      
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ### AI/BI Genie Tool
+
+# COMMAND ----------
+
+
